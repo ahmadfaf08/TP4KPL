f62ae8f3ca59e179f5d23b3f67daf15013c9b9d4
ahmadfaf08 <afafgaming123@gmail.com>
2022-03-18 15:40:08 +0700

    menambahkan class DataGeneric

--- tpmodul4_1302200084/tpmodul4_1302200084/Program.cs
+++ tpmodul4_1302200084/tpmodul4_1302200084/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class HaloGeneric
+{
+    public static void SapaUser<R>(R nama)
+    {
+        Console.WriteLine("Halo User " + nama);
+
+    }
+}
+
+class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("NIM: " + data);
+
+    }
+}
+
+namespace tpmodul4_1302200084
+{
+    internal class Program  
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Nama User: ");
+
+            String a = Console.ReadLine();
+            HaloGeneric.SapaUser<String>(a);
+
+            Console.WriteLine("NIM User: ");
+            String NIM = Console.ReadLine();
+            DataGeneric<String> c = new DataGeneric<string>(NIM);
+            c.PrintData();
+
+        }
+    }
+}
